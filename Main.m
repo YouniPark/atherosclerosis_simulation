e2d1bb2d25f06dae0d3ca5616d5956818f802480
Yoonhee Park <99710920+YouniPark@users.noreply.github.com>
2024-04-07 14:45:02 -0400

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,12 +1,11 @@
 clear
 clc
 
-artery_model = Artery(0.01, 0.01);
+artery_model = Artery(0.9, 0);
 
-% simulate for 0.5s
-t = 0.5;
+% simulate for 0.3s
+t = 0.3;
 [time, state] = artery_model.simulate(t);
-
 
 % calcultate the output y:
 
@@ -15,25 +14,31 @@ R = artery_model.get_resistance;
 Ra = R(1);
 
 % initialize a vector to hold the output
-y = zeros(length(time), 1);
+blood_pressure = zeros(length(time), 1);
+blood_flow = zeros(length(time), 1);
 
 % Loop over each time point
 for i = 1:length(time)
     % Calculate blood flow at the current time point
-    Qt = artery_model.get_blood_flow(time(i));
-    % Calculate y
-    y(i) = state(i,1) + Ra * Qt;
+    blood_flow(i) = artery_model.get_blood_flow(time(i));
+    % Calculate 
+    blood_pressure(i) = state(i,1) + Ra * blood_flow(i);
 end
 
+% Create data and 2-by-1 tiled chart layout
+x = linspace(0,t,length(blood_flow));
+tiledlayout(2,1)
 
+% Plot blood flow
+ax1 = nexttile;
+plot(ax1,x,blood_flow)
+title(ax1,'Blood Flow')
+ylabel(ax1,'dV/dt')
+xlabel(ax1, 'Time (seconds)')
 
-% Plot
-figure()
-LineWidth = 1.5;
-FontSize = 12;
-
-plot(time, y, 'r', 'LineWidth', LineWidth)
-legend('blood pressure') % note the order here
+% Plot blood_pressure
+ax2 = nexttile;
+plot(ax2,x,blood_pressure)
+title(ax2,'Blood Pressure')
+ylabel(ax2,'Pressure (mmHg)')
 xlabel('Time (seconds)')
-ylabel('Pressure (mmHg)')
-set(gca, 'FontSize', FontSize)
